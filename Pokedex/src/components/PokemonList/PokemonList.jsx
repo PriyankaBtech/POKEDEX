c846adf706fb60337479828481befbd3ed4fbb2c
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-10-10 18:18:37 +0530

    advance useState

--- Pokedex/src/components/PokemonList/PokemonList.jsx
+++ Pokedex/src/components/PokemonList/PokemonList.jsx
@@ -5,25 +5,42 @@ import Pokemon from "../Pokemon/Pokemon";'./pokemon';
 
 
 function PokemonList() {
+    
+    // const [pokemonList, setPokemonList] = useState([]);
+    // const [isLoading, setIsLoading] = useState(true)
 
-    const [pokemonList, setPokemonList] = useState([]);
-    const [isLoading, setIsLoading] = useState(true)
+    // const [pokedexUrl, setPokedexUrl] = useState('https://pokeapi.co/api/v2/pokemon');
 
-    const [pokedexUrl, setPokedexUrl] = useState('https://pokeapi.co/api/v2/pokemon');
+    // const [nextUrl, setNextUrl] = useState("");
+    // const [prevUrl, setPrevUrl] = useState("");
 
-    const [nextUrl, setNextUrl] = useState("");
-    const [prevUrl, setPrevUrl] = useState("");
+
+    // Advance useState
+    const [pokemonListState, setPokemonListState] = useState({
+        pokemonList: [],
+        isLoading: true,
+        pokedexUrl: 'https://pokeapi.co/api/v2/pokemon',
+        nextUrl: '',
+        prevUrl: ''
+    });
 
     async function downloadPokemons() {
-        setIsLoading(true);
-        const response = await axios.get(pokedexUrl); // this download the list of 20 pokemons
+        //setIsLoading(true);
+        setPokemonListState((state) => ({ ...state, isLoading: true }));
+
+        const response = await axios.get(pokemonListState.pokedexUrl); // this download the list of 20 pokemons
         //console.log(response) // data
 
         const pokemonResults = response.data.results; // we get the array of pokemons from result
         console.log(response.data); // count
 
-        setNextUrl(response.data.next);
-        setPrevUrl(response.data.previous);
+        // setNextUrl(response.data.next);
+        // setPrevUrl(response.data.previous);
+        setPokemonListState((state) => ({ 
+            ...state,
+            nextUrl: response.data.next,
+            prevUrl: response.data.previous
+        }));
 
         // iterating over the array of pokemons, and using their URL, to create an array of promises
         // that will download those 20 pokemons        
@@ -45,27 +62,38 @@ function PokemonList() {
                 }
         });
         console.log(pokeListResult);
-        setPokemonList(pokeListResult);
-
-        setIsLoading(false);
+        //setPokemonList(pokeListResult);
+        //setIsLoading(false);
+        setPokemonListState((state) => ({
+            ...state,
+            pokemonList: pokeListResult,
+            isLoading: false
+        }));
     }
 
     //useEffect(fn, Array)
     useEffect(() => {
         downloadPokemons();
-    }, [pokedexUrl]);
+    }, [pokemonListState.pokedexUrl]);
     
     return(
         <div className="pokemon-list-wrapper">
        
        <div className="pokemon-wrappper">
-            { (isLoading) ? "Loading..." :
-             pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id} id={p.id} />) }
+            { (pokemonListState.isLoading) ? "Loading..." :
+             pokemonListState.pokemonList.map((p) => <Pokemon name={p.name} image={p.image} key={p.id} id={p.id} />) }
        </div>
 
        <div className="controls">
-           <button disabled={prevUrl == null} onClick={() => setPokedexUrl(prevUrl)} >Prev</button>
-           <button disabled={nextUrl == null} onClick={() => setPokedexUrl(nextUrl)} >Next</button>
+           <button disabled={pokemonListState.prevUrl == null} onClick={() => {
+            const urlToSet = pokemonListState.prevUrl;
+            setPokemonListState({ ...pokemonListState, pokedexUrl: urlToSet})
+           }} >Prev</button>
+
+           <button disabled={pokemonListState.nextUrl == null} onClick={() => {
+            const urlToSet = pokemonListState.nextUrl;
+            setPokemonListState({ ...pokemonListState, pokedexUrl: urlToSet})
+           }} >Next</button>
        </div>
         </div>
     )    
